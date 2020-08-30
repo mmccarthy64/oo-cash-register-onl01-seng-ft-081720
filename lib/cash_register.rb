41636260b3d20ac672892ec00581ee2da6676899
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 21:10:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,7 +31,7 @@ class CashRegister
       @total = @total - (@total * (@discount))
       "After the discount, the total comes to $#{@total.to_i}."
     else
-      puts "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
 
