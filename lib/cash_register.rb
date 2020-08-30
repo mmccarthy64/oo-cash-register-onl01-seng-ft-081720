ac7e52a96d45977e3687b4d2cc63029dcbd789cc
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 21:22:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,19 +11,12 @@ class CashRegister
     @total
   end
   
-  def add_item(title, price, quantity = 1)
-    if quantity > 1
-      i = 0
-      while i<quantity
-        @items << title
-        i += 1
-      end
-    else
-      @items << title
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
     end
-    @total += price*quantity
-    @last_transaction_price = @total
-    @total
+    self.last_transaction = amount * quantity
   end
   
   def apply_discount
