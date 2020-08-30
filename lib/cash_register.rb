213a93493f5ff79d8a0095d62c486d6bf829fad9
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 21:03:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,8 @@ class CashRegister
       @items << title
     end
     @total += price*quantity
-    @last_transaction_price
+    @last_transaction_price = @total
+    @total
   end
   
   def apply_discount
