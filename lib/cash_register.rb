e1ee7ed182e3e91d974673fbf54a266c0aa1519f
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 21:05:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,16 +25,19 @@ class CashRegister
     @total
   end
   
-  def apply_discount
-    
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
   end
-  
-  def items
-    
-  end
-  
-  def void_last_transaction
-    
+
+
+  def void_last_transaction()
+    @total -= @last_transaction_amount
   end
   
 end
